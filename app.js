8c024f110a37a299fdefbf0320bfaa4e1a994b3b
AgosA99 <agosalaniz99@gmail.com>
2025-07-11 23:56:42 -0300

    Agregar checkbox para completar tareas

--- app.js
+++ app.js
@@ -7,19 +7,29 @@ const lista = document.getElementById("lista");
 
 //Eliminar tarea por índice
 const eliminarTarea = (index) => {
-  tareas.splice(index, 1);
-  mostrarLista();
+    tareas.splice(index, 1);
+    mostrarLista();
 };
 
 //Funcion mostrar tareas
 const mostrarLista = () => {
     lista.innerHTML = tareas.map((tarea, index) =>
-    `<div class = "tarjeta">${tarea}
+    `<div class = "tarjeta">
+    <label>
+        <input type="checkbox" onchange="toggleCompletada(${index})" ${tarea.completada ? "checked" : ""}>
+        <span class="${tarea.completada ? "tachado" : ""}">${tarea.texto}</span>
+    </label>
     <br>
     <button onclick="eliminarTarea(${index})">🗑️ Eliminar</button>
     </div>`
     ).join("");
 };
+
+// ✅ Marcar tarea como completada
+function toggleCompletada(index) {
+    tareas[index].completada = !tareas[index].completada;
+    mostrarLista();
+}
 
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -27,7 +37,7 @@ formulario.addEventListener("submit", (e) => {
     const texto = input.value.trim();
     if (texto === "") return; 
 
-    tareas.push(texto);
+    tareas.push({ texto: texto, completada: false });
     input.value = "";
     mostrarLista();  
 })
