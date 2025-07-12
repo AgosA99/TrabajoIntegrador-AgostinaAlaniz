2973b7658b0c8bd2c916a43c25350280cc71af21
AgosA99 <agosalaniz99@gmail.com>
2025-07-11 23:37:40 -0300

    Funcion de eliminar tarea

--- app.js
+++ app.js
@@ -4,15 +4,26 @@ const formulario = document.getElementById("formulario");
 const input = document.getElementById("input-texto");
 const lista = document.getElementById("lista");
 
+
+//Eliminar tarea por índice
+const eliminarTarea = (index) => {
+  tareas.splice(index, 1);
+  mostrarLista();
+};
+
+//Funcion mostrar tareas
 const mostrarLista = () => {
-    lista.innerHTML = tareas.map((tarea) =>
-        <div class = "tarjeta">${tarea}</div>
+    lista.innerHTML = tareas.map((tarea, index) =>
+    `<div class = "tarjeta">${tarea}
+    <br>
+    <button onclick="eliminarTarea(${index})">🗑️ Eliminar</button>
+    </div>`
     ).join("");
 };
 
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
-    
+
     const texto = input.value.trim();
     if (texto === "") return; 
 
